a4377db129359347840e5c3f301a8e147c8dd664
123ghhg <120000173+123ghhg@users.noreply.github.com>
2023-01-05 12:41:33 +0530

    Add files via upload

---
+++ ownprgrm.py
@@ -0,0 +1,13 @@
+x=input("enter for the movie: ")
+k=("kannada")
+y=("tml")
+z=("mlym")
+if x==x:
+    x==x
+    print("you choosen for kannada")
+elif x==y:
+    print("you choosen for tml movie")
+elif x==z:
+    print("you choosen for mlylm movie")
+else:
+    print("sorry ,ur choice is not here")
